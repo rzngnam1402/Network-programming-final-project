380460d052ca906323be42c704c30ba57c50cdad
huuson9 <huuson99812002@gmail.com>
2023-12-14 00:42:55 +0700

    feature/search_by_filename

--- ServerSide/FTP_Server.c
+++ ServerSide/FTP_Server.c
@@ -1,5 +1,10 @@
 #include "FTP_Server.h"
-
+//result of search file
+typedef struct
+{
+	int count;
+	char **files;
+} SearchResult;
 /**
  * Trim whiteshpace and line ending
  * characters from a string
@@ -250,7 +255,7 @@ int ftserve_recv_cmd(int sock_control, char *cmd, char *arg)
 			 (strcmp(cmd, "CWD ") == 0) || (strcmp(cmd, "PWD") == 0) ||
 			 (strcmp(cmd, "STOR") == 0) || (strcmp(cmd, "SORT") == 0) ||
 			 (strcmp(cmd, "FOLD") == 0) || (strcmp(cmd, "STOU") == 0) ||
-			 (strcmp(cmd, "MRET") == 0))
+			 (strcmp(cmd, "MRET") == 0) || (strcmp(cmd, "FIND") == 0))
 	{
 		rc = 200;
 	}
@@ -567,6 +572,90 @@ int recvMulti(int sock_control, int sock_data, char *arg)
 	return 0;
 }
 
+SearchResult searchInDirectory(char *dirPath, char *fileName)
+{
+	SearchResult result = {0, NULL};
+
+	DIR *dir;
+	struct dirent *entry;
+	if ((dir = opendir(dirPath)) == NULL)
+	{
+		perror("opendir");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (entry->d_type == DT_DIR)
+		{
+			// Ignore "." and ".." directories
+			if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+			{
+				char path[PATH_MAX];
+				snprintf(path, PATH_MAX, "%s/%s", dirPath, entry->d_name);
+				SearchResult subdirResult = searchInDirectory(path, fileName);
+
+				// Merge the results
+				result.count += subdirResult.count;
+				result.files = realloc(result.files, result.count * sizeof(char *));
+				for (int i = 0; i < subdirResult.count; ++i)
+				{
+					result.files[result.count - subdirResult.count + i] = subdirResult.files[i];
+				}
+
+				free(subdirResult.files);
+			}
+		}
+		else
+		{
+			if (strcmp(entry->d_name, fileName) == 0)
+			{
+				result.count++;
+				result.files = realloc(result.files, result.count * sizeof(char *));
+				result.files[result.count - 1] = (char *)malloc(PATH_MAX);
+				snprintf(result.files[result.count - 1], PATH_MAX, "%s/%s", dirPath, entry->d_name);
+			}
+		}
+	}
+
+	closedir(dir);
+	return result;
+}
+
+/**
+ * Send path of the file in the dirPath
+ * over data connection
+ * Return -1 on error, 0 on success
+ */
+void ftserve_find(int sock_control, int sock_data, char *filename)
+{
+	char curr_dir[MAX_SIZE - 2];
+	memset(curr_dir, 0, MAX_SIZE);
+	getcwd(curr_dir, sizeof(curr_dir));
+	SearchResult result = searchInDirectory(curr_dir, filename);
+
+	// File found
+	if (result.count > 0)
+	{
+		send_response(sock_control, 241);
+		send_response(sock_control, result.count);
+		for (int i = 0; i < result.count; ++i)
+		{
+			strcat(result.files[i], "\n");
+			if (send(sock_data, result.files[i], strlen(result.files[i]), 0) < 0)
+			{
+				perror("error");
+				send_response(sock_control, 550);
+			}
+			free(result.files[i]); // Free each file path
+		}
+	}
+	// File not found
+	else
+		send_response(sock_control, 441);
+	free(result.files); // Free the array of file paths
+}
+
 /**
  * Child process handles connection to client
  */
@@ -634,6 +723,10 @@ void ftserve_process(int sock_control)
 			else if (strcmp(cmd, "RETR") == 0)
 			{ // RETRIEVE: get file
 				ftserve_retr(sock_control, sock_data, arg);
+			}
+			else if (strcmp(cmd, "FIND") == 0)
+			{ // find file
+				ftserve_find(sock_control, sock_data, arg);
 			}
 			else if (strcmp(cmd, "MRET") == 0)
 			{
